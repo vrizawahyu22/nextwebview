fbb8c359306a12b0350af34673d0343e873092fc
Vriza Wahyu Saputra <vrizawahyu1922@gmail.com>
2021-02-23 01:12:10 +0700

    Open Webcam

--- pages/index.js
+++ pages/index.js
@@ -65,25 +65,8 @@
 // }
 
 import React from 'react';
-import CameraFeed from './components/camera-feed';
+import Webcam from "react-webcam";
 
-// Upload to local seaweedFS instance
-const uploadImage = async file => {
-    const formData = new FormData();
-    formData.append('file', file);
+const WebcamComponent = () => <Webcam />;
 
-    // Connect to a seaweedfs instance
-};
-
-function App() {
-    return (
-        <div className="App">
-            <h1>Image capture test</h1>
-            <p>Capture image from USB webcamera and upload to form</p>
-            <CameraFeed sendFile={uploadImage} />
-        </div>
-    );
-}
-
-export default App;
-
+export default WebcamComponent;
