ab5975dd751bdb4e0795695c01f9f12111aeeeb0
Vriza Wahyu Saputra <vrizawahyu1922@gmail.com>
2021-03-07 02:31:52 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,82 @@
-import React, {useEffect, useState} from 'react';
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// import React, {useEffect, useState} from 'react';
+// import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  const [color, setColor] = useState('blue');
+// export default function Home() {
+//   const [color, setColor] = useState('blue');
 
-  useEffect(() => {
-    window.addEventListener("message", function(msg) {
-      setColor(msg)
-    }, false);
-  }, []);
+//   useEffect(() => {
+//     window.addEventListener("message", function(msg) {
+//       setColor(msg)
+//     }, false);
+//   }, []);
 
-  const onPostMessage = () => {
-    window.postMessage("Post message from web", "*");
-  } 
+//   const onPostMessage = () => {
+//     window.postMessage("Post message from web", "*");
+//   } 
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <button onClick={onPostMessage} style={{ backgroundColor: color }}>Halooo</button>
+//   return (
+//     <div className={styles.container}>
+//       <Head>
+//         <title>Create Next App</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+//       <button onClick={onPostMessage} style={{ backgroundColor: color }}>Halooo</button>
 
-      {/* <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+//       {/* <main className={styles.main}>
+//         <h1 className={styles.title}>
+//           Welcome to <a href="https://nextjs.org">Next.js!</a>
+//         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+//         <p className={styles.description}>
+//           Get started by editing{' '}
+//           <code className={styles.code}>pages/index.js</code>
+//         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+//         <div className={styles.grid}>
+//           <a href="https://nextjs.org/docs" className={styles.card}>
+//             <h3>Documentation &rarr;</h3>
+//             <p>Find in-depth information about Next.js features and API.</p>
+//           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+//           <a href="https://nextjs.org/learn" className={styles.card}>
+//             <h3>Learn &rarr;</h3>
+//             <p>Learn about Next.js in an interactive course with quizzes!</p>
+//           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+//           <a
+//             href="https://github.com/vercel/next.js/tree/master/examples"
+//             className={styles.card}
+//           >
+//             <h3>Examples &rarr;</h3>
+//             <p>Discover and deploy boilerplate example Next.js projects.</p>
+//           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main> */}
+//           <a
+//             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+//             className={styles.card}
+//           >
+//             <h3>Deploy &rarr;</h3>
+//             <p>
+//               Instantly deploy your Next.js site to a public URL with Vercel.
+//             </p>
+//           </a>
+//         </div>
+//       </main> */}
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+//       <footer className={styles.footer}>
+//         <a
+//           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Powered by{' '}
+//           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+//         </a>
+//       </footer>
+//     </div>
+//   )
+// }
 
 // import React from 'react';
 // import { ImagePicker, WingBlank, SegmentedControl } from 'antd-mobile';
@@ -173,3 +173,9 @@ export default function Home() {
 //   return <div>{iniToken}, {iniMessage}</div>;
 // }
 // export default App;
+
+export default function Home() {
+  return (
+    <a href="cQuran://app">Halooo</a>
+  )
+}
